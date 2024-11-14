37f9a68370f006549ee736250a1ba774c4168c3d
oolaya <oolaya@ucompensar.edu.co>
2024-11-13 21:39:21 -0500

    ajuestes

--- lib/domain/repositories/UserFullRepository.dart
+++ lib/domain/repositories/UserFullRepository.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_app_ucompensar_dip/domain/entities/User/User.Entity.dart';
 import 'package:flutter_app_ucompensar_dip/domain/entities/UserFull/UserFull.Entity.dart';
 import 'package:flutter_app_ucompensar_dip/domain/repositories/BaseRepository.dart';
 
